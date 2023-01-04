e3b2fbc70ea8bbe6901c721fd763a08ca62d4e7f
DaegyunOh <oooppq@naver.com>
2023-01-04 19:17:04 +0900

    test

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <div>react project test!!!</div>
+  </React.StrictMode>
+);
+
+const chicken = "test";
+const chicken1 = "test";
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDev = (process.env.NODE_ENV || "development") === "development";
+
+module.exports = {
+  mode: isDev ? "development" : "production",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve("./build"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 5000,
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  esmodules: true,
+                  //   browsers: ["last 2 versions"],
+                },
+              },
+            ],
+            ["@babel/preset-react"],
+          ],
+          plugins: [isDev && "react-refresh/babel"].filter(Boolean),
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+    Build Time: ${new Date().toLocaleString()}
+    `,
+      // Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+      // Author: ${childProcess.execSync("git config user.name")}
+    }),
+    new webpack.DefinePlugin({
+      "api.domain": JSON.stringify("http:dev.api.domain.com/"),
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve("./public"),
+    },
+    client: {
+      overlay: true,
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  stats: "errors-only",
+};
