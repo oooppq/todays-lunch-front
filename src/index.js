635ed435b042ac9929bf6dd81d253b74b929b720
DaegyunOh <oooppq@naver.com>
2023-01-04 19:19:53 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div>react project test!!!</div>
+    <div>making react project without using CRA</div>
   </React.StrictMode>
 );
