15ec597fe70937e402b757abae3cdaae9c56c4f5
DaegyunOh <oooppq@naver.com>
2023-05-28 21:36:28 +0900

    BUG #72: cannot update a component error 해결
    상태를 변화시키는 function을 useEffect의 통제 하에 두지 않아
    발생한 문제였다. 상태를 단순 참조하여 rendering을 결정하는 것은 문제가 되지 않으나,
    useEffect 밖에서 상태를 변경하는 것은 렌더링 순서가 꼬일 수도 있고,
    잘못하면 infinite loop에 빠질 수도 있기 때문에 이를 유의해야 한다.

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 // import React, { useEffect } from 'react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../libs/userAuth.helpers';
@@ -11,14 +11,18 @@ const PrivateRoute = ({ redirectPath = '/login', children }) => {
   const refreshInfo = JSON.parse(localStorage.getItem('refreshInfo'));
   const { setAuthInfo } = useAuth();
 
+  useEffect(() => {
+    if (
+      userState !== authStates.AUTHORIZED ||
+      !refreshInfo ||
+      refreshInfo.expireTime < new Date().getTime()
+    )
+      setAuthInfo(authStates.EXPIRED, null, null);
+  }, [userState, refreshInfo, setAuthInfo]);
+
   if (userState === authStates.INITIAL || userState === authStates.PENDING)
     return null;
-  if (
-    userState !== authStates.AUTHORIZED ||
-    !refreshInfo ||
-    refreshInfo.expireTime < new Date().getTime()
-  ) {
-    setAuthInfo(authStates.EXPIRED, null, null);
+  if (userState !== authStates.AUTHORIZED) {
     return <Navigate to={redirectPath} replace />;
   }
 
