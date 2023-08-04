10c4f3f949676a011c5d17b9f8450e976ee0c97c
DaegyunOh <oooppq@naver.com>
2023-08-04 18:34:03 +0900

    STYLE: 마이페이지의 참여한 맛집 디자인 변경

--- src/pages/MyPage/ParticipatingRestaurant/ParticipatingRestaurantElem.jsx
+++ src/pages/MyPage/ParticipatingRestaurant/ParticipatingRestaurantElem.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRoulette, useWish } from '../../../libs/common.helpers';
+// import { useRoulette, useWish } from '../../../libs/common.helpers';
 import { ParticipatingRestaurantLi } from './participatingRestaurant.style';
-import wishIcon from '../../../assets/img/restaurant-heart-icon.svg';
-import rouletteIcon from '../../../assets/img/restaurant-roulette-icon.svg';
-import coloredWishIcon from '../../../assets/img/restaurant-colored-heart-icon.svg';
-import coloredRouletteIcon from '../../../assets/img/restaurant-colored-roulette-icon.svg';
+import reviewIcon from '../../../assets/img/mypage-review-icon.svg';
+import defaultImage from '../../../assets/img/default-image.png';
+import { convertNum } from '../../../libs/utils';
+// import rouletteIcon from '../../../assets/img/restaurant-roulette-icon.svg';
+// import coloredWishIcon from '../../../assets/img/restaurant-colored-heart-icon.svg';
+// import coloredRouletteIcon from '../../../assets/img/restaurant-colored-roulette-icon.svg';
 
 const ParticipatingRestaurantElem = ({ restaurant }) => {
-  const { isInRoulette, pushRoulette } = useRoulette(restaurant.id);
-  const { isWish, pushWish } = useWish(restaurant.id, restaurant.liked);
   const navigate = useNavigate();
 
   return (
@@ -19,7 +19,11 @@ const ParticipatingRestaurantElem = ({ restaurant }) => {
         navigate(`/restaurants/${restaurant.id}`);
       }}
     >
-      <img src={restaurant.imageUrl} alt="" className="restImg" />
+      <img
+        src={restaurant.imageUrl || defaultImage}
+        alt=""
+        className="restImg"
+      />
       <div className="restInfo">
         <div className="restTitle">{restaurant.restaurantName}</div>
         <div className="restTags">
@@ -27,7 +31,12 @@ const ParticipatingRestaurantElem = ({ restaurant }) => {
           <div className="cat locCat">#{restaurant.locationCategory}</div>
         </div>
       </div>
-      <div className="wishAndRoulette">
+      <div className="rating">4.0</div>
+      <div className="review">
+        <img src={reviewIcon} alt="" className="reviewIcon" />
+        <div className="reviewNum">{convertNum(4712)}</div>
+      </div>
+      {/* <div className="wishAndRoulette">
         <button
           className={isWish ? 'colored' : null}
           type="button"
@@ -48,7 +57,7 @@ const ParticipatingRestaurantElem = ({ restaurant }) => {
         >
           <img src={isInRoulette ? coloredRouletteIcon : rouletteIcon} alt="" />
         </button>
-      </div>
+      </div> */}
     </ParticipatingRestaurantLi>
   );
 };
