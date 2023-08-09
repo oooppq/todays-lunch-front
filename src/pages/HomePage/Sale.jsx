0d02b1950061d45c5277351def74c0af9a2e7613
DaegyunOh <oooppq@naver.com>
2023-08-10 00:50:51 +0900

    FEAT: 홈페이지 세일정보 pagination 추가

--- src/pages/HomePage/Sale.jsx
+++ src/pages/HomePage/Sale.jsx
@@ -1,20 +1,42 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { SaleContainer, SaleTitle, SaleUl, SaleLi } from './homePage.style';
 import defaultImage from '../../assets/img/default-image.png';
-import { convertNum, gotoDetailOnClick } from '../../libs/utils';
+import { convertNum, flattenPages, gotoDetailOnClick } from '../../libs/utils';
+import { useInfiniteScroll } from '../../libs/common.helpers';
 
 const SetData = ({ navigate }) => {
-  // const { isLoading, error, data } = useQuery(['sales', 'list'], () =>
-  const { data } = useQuery(['sales', 'list'], () =>
-    axios
-      .get(`${import.meta.env.VITE_SERVER_URL}/sales`)
-      .then((res) => res.data)
+  const {
+    data: sales,
+    // isFetching: salesIsFetching,
+    // isError: salesIsError,
+    // hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    ['sales', 'list'],
+    ({ pageParam = 1 }) =>
+      axios
+        .get(`${import.meta.env.VITE_SERVER_URL}/sales/?page=${pageParam}`)
+        .then((res) => {
+          return {
+            data: res.data.data,
+            pageNum: pageParam,
+            isLast: pageParam >= res.data.totalPages,
+          };
+        }),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.isLast) return undefined;
+        return lastPage.pageNum + 1;
+      },
+      refetchOnWindowFocus: false,
+    }
   );
 
+  const { observerRef } = useInfiniteScroll(sales, fetchNextPage);
   // if (isLoading) return null;
   // if (error) return 'error';
 
@@ -23,31 +45,30 @@ const SetData = ({ navigate }) => {
       <SaleTitle className="bold">
         💸 할인 <span>정보</span>
       </SaleTitle>
-      <SaleUl>
-        {data
-          ? data.map((e) => (
-              <SaleLi
-                id={e.id}
-                key={e.id}
-                onClick={() => gotoDetailOnClick(e.id, navigate)}
-              >
-                <img src={defaultImage} alt="" />
-                <div className="info">
-                  <div className="storeName">{e.restaurant_name}</div>
-                  <div className="menuName">{e.menu_name}</div>
-                  <div className="priceDiv">
-                    <div className="salePrice">
-                      {convertNum(e.sale_price)}원
-                    </div>
-                    <div className="originalPrice">
-                      {convertNum(e.original_price)}원
-                    </div>
+      {sales && (
+        <SaleUl>
+          {flattenPages(sales.pages).map((e) => (
+            <SaleLi
+              id={e.id}
+              key={e.id}
+              onClick={() => gotoDetailOnClick(e.id, navigate)}
+            >
+              <img src={defaultImage} alt="" />
+              <div className="info">
+                <div className="storeName">{e.restaurant_name}</div>
+                <div className="menuName">{e.menu_name}</div>
+                <div className="priceDiv">
+                  <div className="salePrice">{convertNum(e.sale_price)}원</div>
+                  <div className="originalPrice">
+                    {convertNum(e.original_price)}원
                   </div>
                 </div>
-              </SaleLi>
-            ))
-          : null}
-      </SaleUl>
+              </div>
+            </SaleLi>
+          ))}
+          <div ref={observerRef} className="observer" />
+        </SaleUl>
+      )}
     </SaleContainer>
   );
 };
